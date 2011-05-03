9f060674dfb89c3414bc875c227cb888afb2b02d
mono <mono@monoweb.info>
2011-05-03 15:34:51 +0900

    Added exuberant-ctags

--- build-env/01_apt.sh
+++ build-env/01_apt.sh
@@ -7,7 +7,7 @@ add-apt-repository ppa:nginx/stable
 apt-get update
 apt-get upgrade -y
 
-apt-get install -y build-essential zsh git-core vim curl htop tmux ufw tree
+apt-get install -y build-essential zsh git-core vim curl htop tmux ufw tree exuberant-ctags
 apt-get install -y daemontools-run
 apt-get install -y bison openssl libreadline6 libreadline6-dev zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev
 
