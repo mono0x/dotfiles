96d92cb2c62e9257f9c3751fe62b943ed3f96696
mono <mono@monoweb.info>
2011-11-24 02:17:40 +0900

    Updated ruby

--- build-env/02_rvm.sh
+++ build-env/02_rvm.sh
@@ -2,8 +2,8 @@
 
 bash < <(curl -s https://rvm.beginrescueend.com/install/rvm)
 source ~/.rvm/scripts/rvm
-rvm install 1.9.2
-rvm 1.9.2 --default
+rvm install 1.9.3
+rvm 1.9.3 --default
 
 gem install bundler
 
