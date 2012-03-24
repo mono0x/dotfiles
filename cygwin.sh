76a13bba235eacb3d0de6002f8c93d1962910bb7
mono <mono@monoweb.info>
2012-03-24 23:47:34 +0900

    Add git settings

--- cygwin.sh
+++ cygwin.sh
@@ -3,4 +3,6 @@ cd $HOME
 export CYGWIN=nodosfilewarning
 export LC_ALL=$LANG
 export PATH=/usr/local/bin:/usr/bin:/usr/sbin:$PATH
+export GIT_SSH=~/bin/putty-0.60/plink.exe
+export GIT_EDITOR=~/bin/vim/gvim.exe
 exec /bin/zsh
