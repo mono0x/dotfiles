bcd015726dad1cbf74e4bb983839449a063c7870
mono <mono@monoweb.info>
2012-03-24 22:15:43 +0900

    Add a setup script for Windows

---
+++ build-env/windows/link.cmd
@@ -0,0 +1,9 @@
+mklink %HOME%\_vimrc %HOME%\dotfiles\.vimrc
+mklink %HOME%\_gvimrc %HOME%\dotfiles\.gvimrc
+mklink /D %HOME%\.vim %HOME%\dotfiles\.vim
+mklink %HOME%\.zshrc %HOME%\dotfiles\.zshrc
+mklink %HOME%\.zshenv %HOME%\dotfiles\.zshenv
+mklink /D %HOME%\.zsh %HOME%\dotfiles\.zsh
+mklink %HOME%\.minttyrc %HOME%\dotfiles\.minttyrc
+mklink %HOME%\.gitconfig %HOME%\dotfiles\.gitconfig
+mklink %HOME%\.gitignore %HOME%\dotfiles\.gitignore.global
