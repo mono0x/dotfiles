d945f44fa5c5e835deccc40bfe6f4e47ce41e854
mono <mono@monoweb.info>
2012-09-22 22:16:33 +0900

    Add PostgreSQL

--- build-env/ubuntu/01_apt.sh
+++ build-env/ubuntu/01_apt.sh
@@ -7,4 +7,4 @@ apt-get upgrade -y
 
 apt-get install -y build-essential zsh git-core vim curl htop tmux ufw tree exuberant-ctags tig
 apt-get install -y daemontools-run
-apt-get install -y openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion clang
+apt-get install -y openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion clang postgresql postgresql-server-dev-all
