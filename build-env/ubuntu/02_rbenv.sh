d0d613cb1c2e0a97be43fb6febfb1817f3d76ffe
mono <mono@monoweb.info>
2012-09-26 20:33:03 +0900

    Fix

--- build-env/ubuntu/02_rbenv.sh
+++ build-env/ubuntu/02_rbenv.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
-cd -p ~/.rbenv/plugins
-git clone https://github.com/sstephenson/ruby-build.git
+mkdir -p ~/.rbenv/plugins
+git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 rbenv install 1.9.3-p194
 
 rbenv global 1.9.3-p194
