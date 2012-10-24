232c1d883a8cd6d893cf5bce9c679c1667c678f0
mono <mono@monoweb.info>
2012-10-24 23:36:58 +0900

    Add ruby-1.8.7

--- build-env/ubuntu/02_rbenv.sh
+++ build-env/ubuntu/02_rbenv.sh
@@ -3,6 +3,7 @@
 git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
 mkdir -p ~/.rbenv/plugins
 git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+rbenv install 1.8.7-p370
 rbenv install 1.9.3-p286
 
 rbenv global 1.9.3-p286
