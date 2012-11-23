7542bc5e9f229302177b1fb548be6d4cd0416f51
mono <mono@monoweb.info>
2012-11-24 00:29:52 +0900

    Add zsh-completion of homebrew

--- build-env/macosx/01_homebrew.sh
+++ build-env/macosx/01_homebrew.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 ruby -e "$(curl -fsSkL raw.github.com/mxcl/homebrew/go)"
+ln -s /usr/local/Library/Contributions/brew_zsh_completion.zsh /usr/local/share/zsh/site-functions/_brew
 brew install hub
