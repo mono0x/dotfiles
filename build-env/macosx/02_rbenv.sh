d44045e317fce4c74df8caba24d599a5cc75cf7f
mono <mono@monoweb.info>
2013-04-04 22:57:47 +0900

    Add Ruby 1.9.3

--- build-env/macosx/02_rbenv.sh
+++ build-env/macosx/02_rbenv.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+RUBY_CONFIGURE_OPTS="--enable-shared --with-readline-dir=$(brew --prefix readline) --with-openssl-dir=$(brew --prefix openssl) --with-libyaml-dir=$(brew --prefix libyaml)" CC=gcc rbenv install 1.9.3-p392
 RUBY_CONFIGURE_OPTS="--with-readline-dir=`brew --prefix readline` --with-openssl-dir=`brew --prefix openssl`" rbenv install 2.0.0-p0
 gem install bundler --pre
 rbenv rehash
