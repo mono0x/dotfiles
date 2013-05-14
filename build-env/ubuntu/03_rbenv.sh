df73c2e23d2f66c36b1b179889e998359203bc73
mono <mono@monoweb.info>
2013-05-15 03:37:38 +0900

    Upgrade ruby

--- build-env/ubuntu/03_rbenv.sh
+++ build-env/ubuntu/03_rbenv.sh
@@ -4,8 +4,8 @@ git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
 mkdir -p ~/.rbenv/plugins
 git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 rbenv install 1.8.7-p371
-rbenv install 1.9.3-p362
+rbenv install 2.0.0-p195
 
-rbenv global 1.9.3-p362
+rbenv global 2.0.0-p195
 
 gem install bundler
