2870d0745a3b34465c647eb460335171a2beeeaf
mono <mono@monoweb.info>
2013-08-20 17:22:45 +0900

    Update

--- build-env/ubuntu/06_nvm.sh
+++ build-env/ubuntu/06_nvm.sh
@@ -1,6 +1,8 @@
 #!/bin/zsh
 git clone https://github.com/creationix/nvm.git ~/.nvm
 source ~/.nvm/nvm.sh
-nvm install v0.8.11
-nvm use v0.8.11
+nvm install 0.11
+nvm use 0.11
+nvm alias default 0.11
 npm install -g jshint
+npm install -g bower
