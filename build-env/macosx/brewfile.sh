072ba06303adf01932f362204366a4a8e0113377
mono <mono@monoweb.info>
2014-10-09 13:59:42 +0900

    Use brewfile.sh instead of Brewfile

---
+++ build-env/macosx/brewfile.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+brew update
+brew upgrade
+
+brew install ansible
+brew install git
+brew install gzip
+brew install htop-osx
+brew install openssl
+brew install peco/peco/peco
+brew install postgresql
+brew install pow
+brew install rbenv
+brew install readline
+brew install ruby-build
+brew install the_silver_searcher
+brew install tmux
+brew install wget
+brew install xz
+brew install zsh
+
+brew install caskroom/cask/brew-cask
+brew tap caskroom/versions
+
+brew cask install appcleaner
+brew cask install bittorrent-sync
+brew cask install boot2docker
+brew cask install dropbox
+brew cask install firefox-ja
+brew cask install google-chrome
+brew cask install google-japanese-ime
+brew cask install heroku-toolbelt
+#brew cask install macvim-kaoriya
+brew cask install mendeley-desktop
+brew cask install onepassword
+brew cask install skype
+brew cask install vagrant
+brew cask install virtualbox
+#brew cask install vmware-fusion
+
+brew cask cleanup
+brew cleanup
