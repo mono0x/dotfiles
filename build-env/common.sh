f9fddc0ff059d3e9bddcdfbde3cb4709e9e2e9bd
mono <mono@monoweb.info>
2014-10-27 23:46:59 +0900

    Remove link to ~/bin

--- build-env/common.sh
+++ build-env/common.sh
@@ -24,8 +24,6 @@ ln -s  $dir/.zshenv     ~
 ln -s  $dir/.zshrc      ~
 ln -s  $dir/.tigrc      ~
 
-ln -s  $dir/bin         ~
-
 mkdir ~/.vimswap
 mkdir ~/.vim/bundle
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
