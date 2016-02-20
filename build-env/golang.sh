f620336182200dc65737e38a5e2f3fdf01291cc6
mono <mono0x@users.noreply.github.com>
2016-02-20 15:34:58 +0900

    Add go-server-starter

--- build-env/golang.sh
+++ build-env/golang.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+go get -u github.com/lestrrat/go-server-starter/cmd/start_server
 go get -u github.com/nsf/gocode
 go get -u golang.org/x/tools/cmd/goimports
 go get -u golang.org/x/tools/cmd/guru
