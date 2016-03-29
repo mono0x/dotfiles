e1a2463833789e2be6f895417fc055abcb5bcd56
mono <mono0x@users.noreply.github.com>
2016-03-29 12:23:48 +0900

    Install gorename

--- build-env/golang.sh
+++ build-env/golang.sh
@@ -2,4 +2,5 @@
 go get -u github.com/lestrrat/go-server-starter/cmd/start_server
 go get -u github.com/nsf/gocode
 go get -u golang.org/x/tools/cmd/goimports
+go get -u golang.org/x/tools/cmd/gorename
 go get -u golang.org/x/tools/cmd/guru
