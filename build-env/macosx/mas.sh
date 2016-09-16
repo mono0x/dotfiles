9ed96ad7ffe872b4826c7bb67c0b9ddd89230621
mono <mono@mono0x.net>
2016-09-16 20:59:56 +0900

    Update mas settings

--- build-env/macosx/mas.sh
+++ build-env/macosx/mas.sh
@@ -3,12 +3,7 @@ set -e
 
 mas upgrade
 
-mas install 715768417 # Microsoft Remote Desktop
-mas install 407963104 # Pixelmator
-mas install 408981434 # iMovie
 mas install 497799835 # Xcode
-mas install 409201541 # Pages
 mas install 539883307 # LINE
 mas install 409183694 # Keynote
 mas install 803453959 # Slack
-
