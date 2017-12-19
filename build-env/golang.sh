0aaee86b469e6e61001ecb0f522c52a19c1aeb55
mono <mono@mono0x.net>
2017-12-19 16:46:57 +0900

    Add retool

--- build-env/golang.sh
+++ build-env/golang.sh
@@ -5,6 +5,7 @@ go get -u github.com/golang/lint/golint
 go get -u github.com/lestrrat/go-server-starter/cmd/start_server
 go get -u github.com/mattn/qq/...
 go get -u github.com/nsf/gocode
+go get -u github.com/twitchtv/retool
 go get -u github.com/zmb3/gogetdoc
 go get -u golang.org/x/tools/cmd/goimports
 go get -u golang.org/x/tools/cmd/gorename
