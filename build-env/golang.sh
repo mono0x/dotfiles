e3bb4ae62bf8d73830d229e0ec4b3fb7ae3a2444
mono <mono@mono0x.net>
2019-09-09 15:24:10 +0900

    Use gofumpt instead of goimports

--- build-env/golang.sh
+++ build-env/golang.sh
@@ -4,8 +4,9 @@ go get -u \
     github.com/fatih/motion \
     github.com/zmb3/gogetdoc \
     golang.org/x/lint/golint \
-    golang.org/x/tools/cmd/goimports \
     golang.org/x/tools/cmd/gopls \
     golang.org/x/tools/cmd/gorename \
     golang.org/x/tools/cmd/guru \
-    honnef.co/go/tools/cmd/...
+    honnef.co/go/tools/cmd/... \
+    mvdan.cc/gofumpt \
+    mvdan.cc/gofumpt/gofumports
