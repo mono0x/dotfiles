0b70f19e7a41a52bb699b750199c02a2ae9ebfe8
mono <mono@mono0x.net>
2019-12-02 23:58:52 +0900

    Make symbolic link to diff-highlight

---
+++ build-env/macos/link.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+if [ -f /usr/local/share/git-core/contrib/diff-highlight/diff-highlight ]
+then
+  ln -s /usr/local/share/git-core/contrib/diff-highlight/diff-highlight /usr/local/bin/diff-highlight
+fi
