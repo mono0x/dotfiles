575c76ba9c10dc606c347549a70e25dc8fa4417d
mono <mono@mono0x.net>
2020-01-22 23:56:51 +0900

    Change path to gopls

--- build-env/golang.sh
+++ build-env/golang.sh
@@ -4,7 +4,7 @@ go get -u \
     github.com/fatih/motion \
     github.com/zmb3/gogetdoc \
     golang.org/x/lint/golint \
-    golang.org/x/tools/cmd/gopls \
+    golang.org/x/tools/gopls \
     golang.org/x/tools/cmd/gorename \
     golang.org/x/tools/cmd/guru \
     honnef.co/go/tools/cmd/... \
