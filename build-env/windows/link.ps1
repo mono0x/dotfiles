897f83490924e53bdc41c0245c9261ed325631e2
mono <mono@mono0x.net>
2020-02-11 18:46:58 +0900

    Fix

--- build-env/windows/link.ps1
+++ build-env/windows/link.ps1
@@ -1,5 +1,6 @@
-New-Item -ItemType SymbolicLink -Force -Path ${Home}\.gitconfig -Target ${Home}\dotfiles\.gitconfig
-New-Item -ItemType SymbolicLink -Force -Path ${Home}\.gitconfig.platform -Target ${Home}\dotfiles\.gitconfig.windows
-New-Item -ItemType SymbolicLink -Force -Path ${Home}\.gitignore -Target ${Home}\dotfiles\.gitignore.global
+dir = "${PSScriptRoot}\..\.."
+New-Item -ItemType SymbolicLink -Force -Path ${Home}\.gitconfig -Target "${dir}\.gitconfig"
+New-Item -ItemType SymbolicLink -Force -Path ${Home}\.gitconfig.platform -Target "${dir}\.gitconfig.windows"
+New-Item -ItemType SymbolicLink -Force -Path ${Home}\.gitignore -Target "${dir}\.gitignore.global"
 New-Item -ItemType Directory -Force -Path ${Home}\AppData\Roaming\Code\User
-New-Item -ItemType SymbolicLink -Force -Path ${Home}\AppData\Roaming\Code\User\settings.json -Target ${Home}\dotfiles\vscode\settings.json
+New-Item -ItemType SymbolicLink -Force -Path ${Home}\AppData\Roaming\Code\User\settings.json -Target "${dir}\vscode\settings.json"
