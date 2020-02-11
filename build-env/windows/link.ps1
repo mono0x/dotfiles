b663077b1736e4b02ec169cc4a773a04c2dcae30
mono <mono@mono0x.net>
2020-02-11 18:28:14 +0900

    Update for Windows

--- build-env/windows/link.ps1
+++ build-env/windows/link.ps1
@@ -1,10 +1,4 @@
-New-Item -ItemType SymbolicLink -Path ${Home}\_vimrc -Target ${Home}\dotfiles\.vimrc
-New-Item -ItemType SymbolicLink -Path ${Home}\_gvimrc -Target ${Home}\dotfiles\.gvimrc
-New-Item -ItemType SymbolicLink -Path ${Home}\.vim -Target ${Home}\dotfiles\.vim
-New-Item -ItemType SymbolicLink -Path ${Home}\.zshrc -Target ${Home}\dotfiles\.zshrc
-New-Item -ItemType SymbolicLink -Path ${Home}\.zshenv -Target ${Home}\dotfiles\.zshenv
-New-Item -ItemType SymbolicLink -Path ${Home}\.zsh -Target ${Home}\dotfiles\.zsh
-New-Item -ItemType SymbolicLink -Path ${Home}\.gitconfig -Target ${Home}\dotfiles\.gitconfig
-New-Item -ItemType SymbolicLink -Path ${Home}\.gitconfig.platform -Target ${Home}\dotfiles\.gitconfig.windows
-New-Item -ItemType SymbolicLink -Path ${Home}\.gitignore -Target ${Home}\dotfiles\.gitignore.global
-New-Item -ItemType SymbolicLink -Path ${Home}\AppData\Roaming\Code\User -Target ${Home}\dotfiles\vscode
+New-Item -ItemType SymbolicLink -Force -Path ${Home}\.gitconfig -Target ${Home}\dotfiles\.gitconfig
+New-Item -ItemType SymbolicLink -Force -Path ${Home}\.gitconfig.platform -Target ${Home}\dotfiles\.gitconfig.windows
+New-Item -ItemType SymbolicLink -Force -Path ${Home}\.gitignore -Target ${Home}\dotfiles\.gitignore.global
+New-Item -ItemType SymbolicLink -Force -Path ${Home}\AppData\Roaming\Code\User\settings.json -Target ${Home}\dotfiles\vscode\settings.json
