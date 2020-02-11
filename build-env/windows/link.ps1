e6991b30d22b755a7092a3443e8f6235072799a2
mono <mono@mono0x.net>
2020-02-11 19:39:25 +0900

    Use FullName

--- build-env/windows/link.ps1
+++ build-env/windows/link.ps1
@@ -1,7 +1,6 @@
 $ErrorActionPreference = "Stop"
 
-$dir = (Get-Item (Resolve-Path $PSScriptRoot)).parent.parent
-$dir
+$dir = (Get-Item $PSScriptRoot).parent.parent.FullName
 
 New-Item -ItemType SymbolicLink -Force -Path (Join-Path ${Home} .gitconfig) -Target (Join-Path ${dir} .gitconfig -Resolve)
 New-Item -ItemType SymbolicLink -Force -Path (Join-Path ${Home} .gitconfig.platform) -Target (Join-Path ${dir} .gitconfig.windows -Resolve)
