927ad25d0ef9a9d56fc9c742c2bd20dee0897adb
mono <mono@mono0x.net>
2020-02-11 18:31:48 +0900

    Create directory

--- build-env/windows/link.ps1
+++ build-env/windows/link.ps1
@@ -1,4 +1,5 @@
 New-Item -ItemType SymbolicLink -Force -Path ${Home}\.gitconfig -Target ${Home}\dotfiles\.gitconfig
 New-Item -ItemType SymbolicLink -Force -Path ${Home}\.gitconfig.platform -Target ${Home}\dotfiles\.gitconfig.windows
 New-Item -ItemType SymbolicLink -Force -Path ${Home}\.gitignore -Target ${Home}\dotfiles\.gitignore.global
+New-Item -ItemType Directory -Force -Path ${Home}\AppData\Roaming\Code\User
 New-Item -ItemType SymbolicLink -Force -Path ${Home}\AppData\Roaming\Code\User\settings.json -Target ${Home}\dotfiles\vscode\settings.json
