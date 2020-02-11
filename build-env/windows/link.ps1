31267532e947e17cfd543594b070ba0761b2fe65
mono <mono@mono0x.net>
2020-02-11 19:05:13 +0900

    Fix

--- build-env/windows/link.ps1
+++ build-env/windows/link.ps1
@@ -1,6 +1,6 @@
-dir = "${PSScriptRoot}\..\.."
-New-Item -ItemType SymbolicLink -Force -Path ${Home}\.gitconfig -Target "${dir}\.gitconfig"
-New-Item -ItemType SymbolicLink -Force -Path ${Home}\.gitconfig.platform -Target "${dir}\.gitconfig.windows"
-New-Item -ItemType SymbolicLink -Force -Path ${Home}\.gitignore -Target "${dir}\.gitignore.global"
-New-Item -ItemType Directory -Force -Path ${Home}\AppData\Roaming\Code\User
-New-Item -ItemType SymbolicLink -Force -Path ${Home}\AppData\Roaming\Code\User\settings.json -Target "${dir}\vscode\settings.json"
+$dir = (Get-Item $PSScriptRoot).parent.parent
+New-Item -ItemType SymbolicLink -Force -Path (Join-Path ${Home} .gitconfig) -Target (Join-Path ${dir} .gitconfig)
+New-Item -ItemType SymbolicLink -Force -Path (Join-Path ${Home} .gitconfig.platform) -Target (Join-Path ${dir} .gitconfig.windows)
+New-Item -ItemType SymbolicLink -Force -Path (Join-Path ${Home} .gitignore) -Target (Join-Path ${dir} .gitignore.global)
+New-Item -ItemType Directory -Force -Path (Join-Path ${Home} AppData\Roaming\Code\User)
+New-Item -ItemType SymbolicLink -Force -Path (Join-Path ${Home} AppData\Roaming\Code\User\settings.json) -Target (Join-Path ${dir} vscode\settings.json)
