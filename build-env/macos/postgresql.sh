c6d3770aaa1db3795599d89a9b0287ce28fe214f
mono <mono@mono0x.net>
2020-06-25 20:37:10 +0900

    Remove unnecessary files

--- build-env/macos/postgresql.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-rm -rf /usr/local/var/postgres/*
-initdb --locale=C -E UTF8 /usr/local/var/postgres
-
-# To have launchd start postgresql at login:
-ln -sfv /usr/local/opt/postgresql/*.plist ~/Library/LaunchAgents
-# Then to load postgresql now:
-launchctl load ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist
