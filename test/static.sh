781217bc797f2b2abc319d139722243e714d51a8
mono <mono@mono0x.net>
2020-06-27 13:50:21 +0900

    Skip to install Homebrew for better performance

--- test/static.sh
+++ test/static.sh
@@ -4,4 +4,4 @@ set -e
 cd "$(cd "$(dirname "$0")/.."; pwd)"
 
 shellcheck ./**/*.sh
-zsh -n .zshenv .zshrc
+(command -v zsh > /dev/null) && zsh -n .zshenv .zshrc
--- test/unix.sh
+++ test/unix.sh
@@ -6,3 +6,5 @@ cd "$(cd "$(dirname "$0")/.."; pwd)"
 ./build-env/common.sh
 # Run script twice to test idempotency
 ./build-env/common.sh
+
+(command -v zsh > /dev/null) && zsh -i -c exit
