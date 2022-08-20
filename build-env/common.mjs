9f287e1bd37b58aba2f16ff1b12a1c84f4a4bf97
mono <mono@mono0x.net>
2022-08-21 02:57:05 +0900

    Add missing await

--- build-env/common.mjs
+++ build-env/common.mjs
@@ -24,10 +24,12 @@ const rcs = [
   ".zshrc",
 ]
 
-rcs.forEach(rc => {
-  fs.ensureSymlink(path.join(root, rc), path.join(os.homedir(), rc))
-})
-fs.ensureSymlink(path.join(root, ".gitignore.global"), path.join(os.homedir(), ".gitignore"))
+await Promise.all(
+  rcs.map(rc => {
+    fs.ensureSymlink(path.join(root, rc), path.join(os.homedir(), rc))
+  })
+)
+await fs.ensureSymlink(path.join(root, ".gitignore.global"), path.join(os.homedir(), ".gitignore"))
 
 switch (os.type()) {
   case "Linux":
@@ -35,25 +37,27 @@ switch (os.type()) {
     break
   case "Darwin":
     fs.ensureSymlink(path.join(root, ".gitconfig.macos"), path.join(os.homedir(), ".gitconfig.platform"))
-
-    const cmd = [
-      "/opt/homebrew/share/git-core/contrib/diff-highlight/diff-highlight",
-      "/usr/local/share/git-core/contrib/diff-highlight/diff-highlight",
-    ].find(p => fs.pathExists(p))
-
-    if (cmd) {
-      fs.ensureSymlink(cmd, path.join(os.homedir(), "bin", "diff-highlight"))
-    }
     break
 }
 
-fs.ensureSymlink(path.join(root, "nvim"), path.join(os.homedir(), ".config", "nvim"))
+for (const cmd of [
+  "/home/linuxbrew/.linuxbrew/share/git-core/contrib/diff-highlight/diff-highlight",
+  "/opt/homebrew/share/git-core/contrib/diff-highlight/diff-highlight",
+  "/usr/local/share/git-core/contrib/diff-highlight/diff-highlight",
+]) {
+  if (await fs.pathExists(cmd)) {
+    await fs.ensureSymlink(cmd, path.join(os.homedir(), "bin", "diff-highlight"))
+    break
+  }
+}
 
-if (!fs.pathExists(path.join(os.homedir(), ".asdf"))) {
+await fs.ensureSymlink(path.join(root, "nvim"), path.join(os.homedir(), ".config", "nvim"))
+
+if (!await fs.pathExists(path.join(os.homedir(), ".asdf"))) {
   await $`git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.0`
 }
 
-if (!fs.pathExists(path.join(os.homedir(), ".zinit"))) {
+if (!await fs.pathExists(path.join(os.homedir(), ".zinit"))) {
   await fs.mkdirp(path.join(os.homedir(), ".zinit"))
   await $`git clone https://github.com/zdharma-continuum/zinit.git ~/.zinit/bin`
 }
