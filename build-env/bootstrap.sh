68ee4a7936a39e2a964e12b20a048157fb6f750f
mono <mono@mono0x.net>
2023-01-02 18:32:41 +0000

    Update

--- build-env/bootstrap.sh
+++ build-env/bootstrap.sh
@@ -12,20 +12,18 @@ dotfiles_dir="$dotfiles_parent/dotfiles"
 
 if [ ! -d "$dotfiles_dir" ]
 then
-  echo "Cloning the repository..." >&2
+  echo "Cloning mono0x/dotfiles..." >&2
   mkdir -p "$dotfiles_parent"
   git clone https://github.com/mono0x/dotfiles "$dotfiles_dir"
 fi
 
 # Homebrew
-if [ ! -f /opt/homebrew/bin/brew ] && [ ! -f /home/linuxbrew/.linuxbrew/bin/brew ]
+if ! command -v brew 2>&1 > /dev/null
 then
   echo "Installing Homebrew..." >&2
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
   . "$dotfiles_dir/bin/homebrew.sh"
 fi
-
-env
 
 brew install zx
 
