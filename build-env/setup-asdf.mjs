9ad99e1312473d6d540298b50562ccdec14532f3
mono <mono@mono0x.net>
2023-01-04 19:11:48 +0000

    Fix when plugin list is empty

--- build-env/setup-asdf.mjs
+++ build-env/setup-asdf.mjs
@@ -1,7 +1,17 @@
 #!/usr/bin/env zx
 /// <reference types="zx/build/globals" />
 
-const installed = (await $`asdf plugin list`).stdout.split("\n")
+const installed = await (async () => {
+  try {
+    return (await $`asdf plugin list`).stdout.split("\n")
+  } catch (e) {
+    if (e.exitCode === 1) {
+      return []
+    } else {
+      throw e
+    }
+  }
+})()
 
 const plugins = [
   "direnv",
