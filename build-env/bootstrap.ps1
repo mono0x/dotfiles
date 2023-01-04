dc86e0b194c50f3a746bbda9dc14f9dcd669c6d4
mono <mono@mono0x.net>
2023-01-04 18:37:21 +0000

    Fix

--- build-env/bootstrap.ps1
+++ build-env/bootstrap.ps1
@@ -1,6 +1,6 @@
 $ErrorActionPreference = "Stop"
 
-if ($env:SKIP_PACKAGE_INSTALLATION -ne $true) {
+if ($env:SKIP_PACKAGE_INSTALLATION -eq $null) {
   Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
   if (-not (Get-Command scoop -ErrorAction SilentlyContinue)) {
     Invoke-RestMethod get.scoop.sh | Invoke-Expression
@@ -15,7 +15,7 @@ if (-not (Test-Path $dotfiles_dir)) {
 }
 Set-Location $dotfiles_dir
 
-if ($env:SKIP_PACKAGE_INSTALLATION -ne $true) {
+if ($env:SKIP_PACKAGE_INSTALLATION -eq $null) {
   scoop import https://raw.githubusercontent.com/mono0x/dotfiles/main/scoopfile.json
   winget import winget.json
 }
