5683026697b40613c7324a42f2e405e8d613a42b
mono <mono@mono0x.net>
2023-04-24 08:27:25 +0000

    Use .chezmoiexternal

--- src/run_once_asdf.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-git clone "https://github.com/asdf-vm/asdf.git" "$HOME/.asdf" || true
--- src/run_once_zinit.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-git clone "https://github.com/zdharma-continuum/zinit.git" "$HOME/.zinit/bin" || true
