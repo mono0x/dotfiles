8446684331dc3829df57062b360e1df0b3c34d60
mono <mono@mono0x.net>
2023-09-26 15:38:01 +0000

    Fix

--- src/run_after_goss.sh
+++ src/run_after_goss.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 set -eu
 cd "$HOME/.local/share/chezmoi"
-GOSS_USE_ALPHA=1 goss validate
+# Calling with absolute path for the case where $HOME/bin is not set in PATH
+GOSS_USE_ALPHA=1 "$HOME/bin/goss" validate
