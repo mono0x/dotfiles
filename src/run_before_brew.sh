a57778419f4a5b0ecc9d7ee8d4710b54d2c7b3d7
mono <mono@mono0x.net>
2023-09-27 23:48:42 +0900

    Fix

--- src/run_before_brew.sh
+++ src/run_before_brew.sh
@@ -1,16 +1,4 @@
 #!/bin/sh
-
-cd "${CHEZMOI_SOURCE_DIR:-$(dirname $0)}/.."
-
-prefix=""
-case "$(uname)" in
-  Darwin)
-    prefix="/opt/homebrew"
-  ;;
-
-  Linux)
-    prefix="/home/linuxbrew/.linuxbrew"
-  ;;
-esac
-
-"$prefix/bin/brew" bundle --no-upgrade
+set -eu
+cd "${CHEZMOI_SOURCE_DIR:-$(dirname "$0")}/.."
+brew bundle --no-upgrade
