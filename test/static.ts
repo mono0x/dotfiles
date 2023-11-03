0bef91f6dfc8297f165c05b0a729db375a5d69c8
mono <mono@mono0x.net>
2023-11-03 22:51:39 +0900

    Fix

--- test/static.ts
+++ test/static.ts
@@ -1,3 +1,5 @@
+import * as path from "https://deno.land/std/path/mod.ts";
+import * as fs from "https://deno.land/std/fs/mod.ts";
 import $ from "https://deno.land/x/dax/mod.ts";
 
 $.setPrintCommand(true);
@@ -8,22 +10,22 @@ if (!homedir) {
   throw new Error("HOME is not set");
 }
 
-const root = $.path.join(
-  $.path.dirname($.path.fromFileUrl(import.meta.url)),
+const root = path.join(
+  path.dirname(path.fromFileUrl(import.meta.url)),
   "..",
 );
 $.cd(root);
 
 await $`shellcheck --version`;
-for await (const file of $.fs.expandGlob("{bin,src}/**/*.sh")) {
+for await (const file of fs.expandGlob("{bin,src}/**/*.sh")) {
   await $`shellcheck ${file.path}`;
 }
-await $`shellcheck ${$.path.join(root, "install.sh")}`;
+await $`shellcheck ${path.join(root, "install.sh")}`;
 
 for (const file of ["src/dot_zshenv"]) {
-  await $`zsh -n ${$.path.join(root, file)}`;
+  await $`zsh -n ${path.join(root, file)}`;
 }
-for await (const file of $.fs.expandGlob("src/**/*.zsh")) {
+for await (const file of fs.expandGlob("src/**/*.zsh")) {
   await $`zsh -n ${file.path}`;
 }
 await $`deno lint`;
