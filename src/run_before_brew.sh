26b0e2724f9cfb580432d90b79030893ab550aca
mono <mono@mono0x.net>
2023-11-28 13:58:01 +0000

    Skip casks on GitHub Actions

--- src/run_before_brew.sh
+++ src/run_before_brew.sh
@@ -4,5 +4,5 @@ set -eu
 if command -v brew > /dev/null 2>&1
 then
   cd "${CHEZMOI_SOURCE_DIR:-$(dirname "$0")}/.."
-  brew bundle --no-upgrade
+  HOMEBREW_BUNDLE_CASK_SKIP="$([ "${GITHUB_ACTIONS:-""}" = "true" ] && echo 1 || echo 0)" brew bundle --no-upgrade
 fi
