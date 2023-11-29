58075f1a23bf0f6680b466047290d1b4312cae86
mono <mono@mono0x.net>
2023-11-30 01:38:05 +0900

    Refactor

--- src/run_before_brew.sh
+++ src/run_before_brew.sh
@@ -4,5 +4,9 @@ set -eu
 if command -v brew > /dev/null 2>&1
 then
   cd "${CHEZMOI_SOURCE_DIR:-$(dirname "$0")}/.."
-  HOMEBREW_BUNDLE_CASK_SKIP="$([ "${GITHUB_ACTIONS:-""}" = "true" ] && echo 1 || echo 0)" brew bundle --no-upgrade
+  if [ "${GITHUB_ACTIONS:-""}" = "true" ]
+  then
+    export HOMEBREW_BUNDLE_CASK_SKIP=1
+  fi
+  brew bundle --no-upgrade
 fi
