f076db515a4a002ca4752ac3231afa0da9fc90a4
mono <mono@mono0x.net>
2024-05-28 16:41:42 +0000

    Refactor install script to use TypeScript

---
+++ _install/main.ts
@@ -0,0 +1,110 @@
+import $ from "@david/dax";
+import * as fs from "@std/fs";
+import * as path from "@std/path";
+
+$.setPrintCommand(true);
+
+if (
+  !(Deno.build.os === "linux" || Deno.build.os === "darwin" ||
+    Deno.build.os === "windows")
+) {
+  console.error(`Unsupported OS: ${Deno.build.os}`);
+  Deno.exit(1);
+}
+
+// const ci = Deno.env.get("GITHUB_ACTIONS") === "true";
+const remoteContainers = Deno.env.get("REMOTE_CONTAINERS") === "true";
+const wsl = !!Deno.env.get("WSL_DISTRO_NAME");
+
+if (remoteContainers) {
+  await setupRemoteContainer();
+  Deno.exit(0);
+}
+
+switch (Deno.build.os) {
+  case "linux":
+  case "darwin":
+    await setupUnix(Deno.build.os);
+    break;
+
+  case "windows":
+    await setupWindows();
+    break;
+}
+
+async function setupRemoteContainer() {
+  await $`sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply --verbose mono0x`;
+}
+
+async function setupUnix(os: "linux" | "darwin") {
+  if (os === "linux") {
+    await aptInstall([
+      // https://docs.brew.sh/Homebrew-on-Linux
+      "build-essential",
+      "procps",
+      "curl",
+      "file",
+      "git",
+
+      "language-pack-en",
+      "libssl-dev",
+      "pkg-config",
+      "zsh",
+    ]);
+  }
+
+  const scoopShimDir = await (async () => {
+    if (!wsl) return;
+    const scoop = await $.which("scoop");
+    return scoop && path.dirname(scoop);
+  })();
+
+  const homebrewDir = os === "linux"
+    ? "/home/linuxbrew/.linuxbrew"
+    : "/opt/homebrew";
+
+  const homeDir = Deno.env.get("HOME");
+  if (!homeDir) {
+    console.error("HOME environment variable not set");
+    Deno.exit(1);
+  }
+
+  const chezomiBinDir = path.join(homeDir, ".local", "bin");
+  if (!await fs.exists(path.join(chezomiBinDir, "chezmoi"))) {
+    await $`sh -c "$(curl -fsLS get.chezmoi.io)" -- -b ${chezomiBinDir}`;
+  }
+
+  if (!await fs.exists(path.join(homebrewDir, "bin", "brew"))) {
+    await $`/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"`;
+  }
+
+  await $`/bin/bash --noprofile --norc`
+    .stdinText(`
+      export PATH=$PATH:${scoopShimDir}
+      eval $("${homebrewDir}/bin/brew" shellenv)
+      "${chezomiBinDir}/chezmoi" init --verbose --apply mono0x
+    `)
+    .clearEnv()
+    .env({
+      HOME: homeDir,
+    });
+}
+
+async function setupWindows() {
+  // TODO
+}
+
+async function aptInstall(packages: string[]) {
+  const installed = new Set(
+    (await $`dpkg --get-selections`.lines())
+      .map((l) => l.split(/\t+/))
+      .filter(([, v]) => v === "install")
+      .map(([k]) => k.includes(":") ? k.split(":")[0] : k),
+  );
+
+  const installs = packages.filter((p) => !installed.has(p));
+
+  if (installs.length > 0) {
+    await $`${Deno.uid() == 0 ? "" : "sudo"} apt-get install -y ${installs}`;
+  }
+}
--- install.sh
+++ install.sh
@@ -3,59 +3,8 @@ set -eu
 
 cd "$(dirname "$0")"
 
-if ! (command -v git > /dev/null 2>&1)
-then
-  echo "git not found." >&2
-  return 1
-fi
-
-if [ "${REMOTE_CONTAINERS:-}" = "true" ]
-then
-  echo "Running in a remote container." >&2
-  sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply --verbose mono0x
-  exit
-fi
-
-HOMEBREW_INSTALL=""
-case "$(uname)" in
-Darwin)
-  HOMEBREW_INSTALL="/opt/homebrew"
-  ;;
-
-Linux)
-  HOMEBREW_INSTALL="/home/linuxbrew/.linuxbrew"
-  ;;
-esac
 DENO_INSTALL="$HOME/.local"
-CHEZMOI_INSTALL="$HOME/.local/bin"
-
-SCOOP_SHIM_DIR=""
-if command -v scoop > /dev/null 2>&1
-then
-  SCOOP_SHIM_DIR="$(dirname "$(which scoop)")"
-fi
-
-command -v "$HOMEBREW_INSTALL/bin/brew" > /dev/null 2>&1 || \
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
 command -v "$DENO_INSTALL/bin/deno" > /dev/null 2>&1 || \
   DENO_INSTALL="$DENO_INSTALL" sh -c "$(curl -fsSL https://deno.land/x/install/install.sh)"
 
-command -v "$CHEZMOI_INSTALL/chezmoi" > /dev/null 2>&1 || \
-  sh -c "$(curl -fsLS get.chezmoi.io)" -- -b "$CHEZMOI_INSTALL"
-
-if [ "$(uname)" = "Linux" ]
-then
-  "$DENO_INSTALL/bin/deno" run -A _install/apt-install.ts \
-    build-essential \
-    language-pack-en \
-    libssl-dev \
-    pkg-config \
-    zsh
-fi
-
-env -i HOME="$HOME" bash --noprofile --norc <<EOS
-  export PATH="\$PATH:$SCOOP_SHIM_DIR"
-  eval "$($HOMEBREW_INSTALL/bin/brew shellenv)"
-  "$CHEZMOI_INSTALL/chezmoi" init --verbose --apply mono0x
-EOS
+exec "$DENO_INSTALL/bin/deno" run -A _install/main.ts
