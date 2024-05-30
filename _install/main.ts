943a77256b4b0c9ef9f44b67d731dbc391a80bd1
mono <mono@mono0x.net>
2024-05-30 12:59:21 +0900

    Tweak

--- _install/main.ts
+++ _install/main.ts
@@ -35,11 +35,20 @@ switch (Deno.build.os) {
 }
 
 async function setupRemoteContainer() {
+  console.log("Installing and initalizing chezmoi");
   await $`sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply --verbose ${account}`;
 }
 
 async function setupUnix(os: "linux" | "darwin") {
+  const homeDir = Deno.env.get("HOME");
+  if (!homeDir) {
+    console.error("HOME environment variable not set");
+    Deno.exit(1);
+  }
+
   if (os === "linux") {
+    console.log("Installing dependencies");
+
     await aptInstall([
       // https://docs.brew.sh/Homebrew-on-Linux
       "build-essential",
@@ -66,15 +75,13 @@ async function setupUnix(os: "linux" | "darwin") {
     : "/opt/homebrew";
   const brew = path.join(homebrewDir, "bin", "brew");
 
-  const homeDir = Deno.env.get("HOME");
-  if (!homeDir) {
-    console.error("HOME environment variable not set");
-    Deno.exit(1);
-  }
+  console.log("Installing Homebrew");
 
   if (!await fs.exists(brew)) {
     await $`/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"`;
   }
+
+  console.log("Installing and initializing chezmoi");
 
   await $`/bin/bash --noprofile --norc`
     .stdinText(`
@@ -90,13 +97,13 @@ async function setupUnix(os: "linux" | "darwin") {
 }
 
 async function setupWindows() {
-  // TODO
-
   const homeDir = Deno.env.get("USERPROFILE");
   if (!homeDir) {
     console.error("USERPROFILE environment variable not set");
     Deno.exit(1);
   }
+
+  console.log("Installing scoop");
 
   const scoopShimDir = path.join(homeDir, "scoop", "shims");
   if (!await fs.exists(scoopShimDir)) {
@@ -106,6 +113,8 @@ async function setupWindows() {
         scoop install chezmoi git
       `);
   }
+
+  console.log("Initializing chezmoi");
 
   const chezmoi = path.join(scoopShimDir, "chezmoi.exe");
   await $`pwsh -noprofile -noninteractive -`
