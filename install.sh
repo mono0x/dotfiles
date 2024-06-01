2089594325761d7c9e70bfc4140a7b7dbad2df44
mono <mono@mono0x.net>
2024-06-01 05:15:06 +0000

    Download config and lock file

--- install.sh
+++ install.sh
@@ -14,7 +14,13 @@ fi
 prefix="$(dirname "$0")"
 if [ ! -f "$prefix/install/main.ts" ]
 then
-  prefix="https://raw.githubusercontent.com/mono0x/dotfiles"
+  dir="$(mktemp --tmpdir -d dotfiles.XXXXXX)"
+  mkdir -p "$dir/install"
+  for file in install/main.ts deno.jsonc deno.lock
+  do
+    curl -fsSL "https://raw.githubusercontent.com/mono0x/dotfiles/main/$file" -o "$dir/$file"
+  done
+  prefix="$dir"
 fi
 
 exec "$deno" run --config "$prefix/deno.jsonc" --lock "$prefix/deno.lock" -A "$prefix/install/main.ts"
