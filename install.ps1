2eb09e6d99e63ebde411469aa416834b103527f3
mono <mono@mono0x.net>
2024-06-01 14:21:27 +0900

    Download config and lock file

--- install.ps1
+++ install.ps1
@@ -1,6 +1,11 @@
 $ErrorActionPreference = "Stop"
 
-Set-Location (Split-Path -Parent $PSCommandPath)
+# https://stackoverflow.com/questions/34559553/create-a-temporary-directory-in-powershell
+function New-TemporaryDirectory {
+  $parent = [System.IO.Path]::GetTempPath()
+  $name = [System.IO.Path]::GetRandomFileName()
+  New-Item -ItemType Directory -Path (Join-Path $parent $name)
+}
 
 $env:DENO_INSTALL = "$env:USERPROFILE\.local"
 if (-not (Test-Path "$env:DENO_INSTALL\bin\deno.exe")) {
@@ -8,8 +13,14 @@ if (-not (Test-Path "$env:DENO_INSTALL\bin\deno.exe")) {
   Invoke-RestMethod https://deno.land/install.ps1 | Invoke-Expression
 }
 
-$prefix = $env:GITHUB_ACTIONS -eq "true" `
-  ?  "." `
-  : "https://raw.githubusercontent.com/mono0x/dotfiles/main/"
+$prefix = Split-Path -Parent $PSCommandPath
+if (-not (Test-Path "$prefix\install\main.ts")) {
+  $dir = New-TemporaryDirectory
+  New-Item -ItemType Directory -Force -Path "$dir\install"
+  foreach ($file in "install/main.ts", "deno.jsonc", "deno.lock") {
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/mono0x/dotfiles/main/$file" -OutFile "$dir\$file"
+  }
+  $prefix = $dir
+}
 
 . "$env:DENO_INSTALL\bin\deno.exe" run -A --config "$prefix/deno.jsonc" --lock "$prefix/deno.lock" "$prefix/install/main.ts"
