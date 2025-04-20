5be526b96a2e63a685c452d39fadf29ff1b39a6c
mono <mono@mono0x.net>
2025-04-20 17:16:04 +0900

    Remove is_darwin condition

--- src/dot_config/wezterm/wezterm.lua
+++ src/dot_config/wezterm/wezterm.lua
@@ -2,8 +2,6 @@
 local wezterm = require 'wezterm'
 local act = wezterm.action
 
-local is_darwin = wezterm.target_triple:find('darwin')
-
 ---@type Config
 local config = wezterm.config_builder()
 config:set_strict_mode(true)
@@ -17,49 +15,45 @@ config.color_scheme = "Builtin Solarized Light"
 config.initial_cols = 120
 config.initial_rows = 40
 config.font = wezterm.font_with_fallback {
-    'HackGen Console NF',
+  'HackGen Console NF',
 }
-if is_darwin then
-    config.font_size = 14.0
-else
-    config.font_size = 12.0
-end
+config.font_size = 14.0
 
 -- Keys
 config.keys = {}
 -- config.debug_key_events = true
 
 local numbers_with_shift = {
-    '!', '"', '#', '$', '%', '&', "'", '(', ')',
+  '!', '"', '#', '$', '%', '&', "'", '(', ')',
 }
 if config.launch_menu ~= nil then
-    for i, v in ipairs(config.launch_menu) do
-        table.insert(config.keys, {
-            key = numbers_with_shift[i],
-            mods = 'CTRL|SHIFT',
-            action = act.SpawnCommandInNewTab {
-                args = v.args,
-            },
-        })
-        table.insert(config.keys, {
-            key = numbers_with_shift[i],
-            mods = 'SUPER|SHIFT',
-            action = act.SpawnCommandInNewTab {
-                args = v.args,
-            },
-        })
-    end
+  for i, v in ipairs(config.launch_menu) do
+    table.insert(config.keys, {
+      key = numbers_with_shift[i],
+      mods = 'CTRL|SHIFT',
+      action = act.SpawnCommandInNewTab {
+        args = v.args,
+      },
+    })
+    table.insert(config.keys, {
+      key = numbers_with_shift[i],
+      mods = 'SUPER|SHIFT',
+      action = act.SpawnCommandInNewTab {
+        args = v.args,
+      },
+    })
+  end
 else
-    table.insert(config.keys, {
-        key = numbers_with_shift[1],
-        mods = 'CTRL|SHIFT',
-        action = act.SpawnTab 'CurrentPaneDomain',
-    })
-    table.insert(config.keys, {
-        key = numbers_with_shift[1],
-        mods = 'SUPER|SHIFT',
-        action = act.SpawnTab 'CurrentPaneDomain',
-    })
+  table.insert(config.keys, {
+    key = numbers_with_shift[1],
+    mods = 'CTRL|SHIFT',
+    action = act.SpawnTab 'CurrentPaneDomain',
+  })
+  table.insert(config.keys, {
+    key = numbers_with_shift[1],
+    mods = 'SUPER|SHIFT',
+    action = act.SpawnTab 'CurrentPaneDomain',
+  })
 end
 
 return config
