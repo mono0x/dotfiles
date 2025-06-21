acb8584fef44785d23440fb5b6556f8967b4e02e
mono <mono@mono0x.net>
2025-06-21 17:20:50 +0900

    Format goss output

--- src/.chezmoiscripts/unix/run_after_goss.sh
+++ src/.chezmoiscripts/unix/run_after_goss.sh
@@ -2,4 +2,4 @@
 set -eu
 cd "$HOME/.local/share/chezmoi"
 # Calling with absolute path for the case where $HOME/bin is not set in PATH
-GOSS_USE_ALPHA=1 "$HOME/bin/goss" validate
+GOSS_USE_ALPHA=1 "$HOME/bin/goss" validate --format documentation
