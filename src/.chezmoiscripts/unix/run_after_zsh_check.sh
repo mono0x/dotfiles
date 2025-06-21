49394f9a55715c87173dc6d08efb9d1216e3e12d
mono <mono@mono0x.net>
2025-06-21 17:07:49 +0900

    Fix

--- src/.chezmoiscripts/unix/run_after_zsh_check.sh
+++ src/.chezmoiscripts/unix/run_after_zsh_check.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 set -eu
-zsh --no-exec \
-  "$HOME/.config/zsh/.zshrc" \
-  "$HOME/.config/zsh/functions/*" \
-  "$HOME/.config/zsh/zshrc.d/*" \
-  "$HOME/.config/zsh/zshrc.defer.d/*" \
-  "$HOME/.zprofile" \
-  "$HOME/.zshenv"
+for file in \
+  "$HOME"/.config/zsh/.zshrc \
+  "$HOME"/.config/zsh/functions/* \
+  "$HOME"/.config/zsh/zshrc.d/* \
+  "$HOME"/.config/zsh/zshrc.defer.d/* \
+  "$HOME"/.zprofile \
+  "$HOME"/.zshenv
+do
+  # `zsh --no-exec` only accepts single file
+  zsh --no-exec "$file"
+done
