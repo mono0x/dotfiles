9dd3fa553c364021ba4219262614c25975c8d56b
mono <mono@mono0x.net>
2025-11-15 00:59:13 +0900

    Refer dotfiles/bin directly from PATH

--- src/dot_config/zsh/zshrc.d/path.zsh
+++ src/dot_config/zsh/zshrc.d/path.zsh
@@ -1,5 +1,6 @@
 path=(
   $path
   $HOME/.rd/bin(N-/) # Rancher Desktop
+  $HOME/.local/share/chezmoi/bin(N-/) # dotfiles
   $HOME/src/github.com/mono0x/utils/bin(N-/) # https://github.com/mono0x/utils (private)
 )
