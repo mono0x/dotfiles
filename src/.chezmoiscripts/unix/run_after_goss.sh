19677d52f965e84927df75fcea2357adbebc534d
mono <mono@mono0x.net>
2025-11-15 11:13:57 +0900

    Fix mise exec

--- src/.chezmoiscripts/unix/run_after_goss.sh
+++ src/.chezmoiscripts/unix/run_after_goss.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -eu
 cd "$HOME/.local/share/chezmoi"
-GOSS_USE_ALPHA=1 mise exec goss -- goss validate --format documentation
+GOSS_USE_ALPHA=1 mise exec github:goss-org/goss -- goss validate --format documentation
